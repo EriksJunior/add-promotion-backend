ab638361f28f48872e35a6127fbdebffbc1827b7
EriksJunior <EriksJunior13@gmail.com>
2022-07-07 23:16:18 -0300

    refactor do refactor

--- src/controllers/CompanyController.ts
+++ src/controllers/CompanyController.ts
@@ -7,32 +7,32 @@ class CompanyController {
     try {
       const body = req.body
       const data = await CompanyService.save(body)
-      
+
       res.status(201).json(data)
     } catch (error: any) {
       res.status(400).json({ error: error })
     }
   }
 
-  async search(req: Request, res: Response){
+  async search(req: Request, res: Response) {
     try {
       const id = req.params.id
       const data = await CompanyService.search(id)
 
-      return res.status(200).json(data)
+      res.status(200).json(data)
     } catch (error: any) {
-      return res.status(400).json({error: error.message})
+      res.status(400).json({ error: error.message })
     }
   }
 
-  async update(req: Request, res: Response){
+  async update(req: Request, res: Response) {
 
   }
 
-  async delete(req: Request, res: Response){
+  async delete(req: Request, res: Response) {
 
   }
-  
+
 }
 
 export default new CompanyController()
