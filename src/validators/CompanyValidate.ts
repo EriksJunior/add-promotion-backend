ffd1f47a5854bed0cf47f68c00c1ec47be576b0c
eriks junior <eriksjunior13@gmail.com>
2022-07-26 21:12:52 -0300

    refactro

--- src/validators/CompanyValidate.ts
+++ src/validators/CompanyValidate.ts
@@ -8,6 +8,7 @@ const companyValidate = Joi.object({
   number: Joi.number().required(),
   city: Joi.string().required().max(50).rule({message: 'Cidade incorreto, deve ter no maximo 50 caracteres'}),
   cep: Joi.string().required().max(9).min(8).rule({message: 'Cep incorreto, deve ter 9 caracteres'}),
+  uf: Joi.string().required().max(2).min(2).rule({message: 'Uf incorreta'}),
   cpfCnpj: Joi.string().required().min(11).max(18).rule({message: 'CpfCnpj incorreto, deve ter entre 14 e 18 caracteres'}),
   email: Joi.string().required().email().rule({message: 'Email deve ser valido, e deve ter no maximo 50 caracteres'}),
   telephone: Joi.string().min(8).max(20).rule({message: 'Email deve ser valido, e deve ter no maximo 50 caracteres'}),
