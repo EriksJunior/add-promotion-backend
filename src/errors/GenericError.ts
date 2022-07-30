e832056b2c9a9634ea3bb555a3e32d2ed2206135
EriksJunior <EriksJunior13@gmail.com>
2022-07-30 11:33:04 -0300

    refactor: adicionao a classe GenericError para extender de Error

--- src/errors/GenericError.ts
+++ src/errors/GenericError.ts
@@ -1,11 +1,13 @@
-export class GenericError {
-
-  database(message: string) {
-    return message
+class GenericError extends Error {
+  erros: any
+  statusCode: number
+  constructor(message: string, erros: any, statusCode: number){
+    super(message)
+    this.erros = erros
+    this.statusCode = statusCode
   }
-
 }
 
-export default new GenericError()
+export {GenericError}
 
 
