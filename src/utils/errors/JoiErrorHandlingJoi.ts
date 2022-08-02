7e1908c9d2faf8d0b0efef30b24faa8926c34b22
eriks junior <eriksjunior13@gmail.com>
2022-08-01 21:15:06 -0300

    refactor: (JoiErrorHandling) agora retorna um array de objetos, com a key de cada error

--- src/utils/errors/JoiErrorHandlingJoi.ts
+++ src/utils/errors/JoiErrorHandlingJoi.ts
@@ -1,11 +1,12 @@
 
-import Joi from "joi";
+import Joi, { object } from "joi";
 
-class JoiErrorHandling{
-   JoiErrorHandling(value: Array<Joi.ValidationErrorItem>){
-    const result = value.map((e) =>{
-        return e.message
-      }) 
+class JoiErrorHandling {
+  JoiErrorHandling(value: Array<Joi.ValidationErrorItem>) {
+    const result = value.map((e) => {
+      return { [e.path as any]: e.message }
+    })
+    
     return result
   }
 }
