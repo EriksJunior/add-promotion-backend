d18c29b12a19b98ed39d3d86bb49e935c264b681
eriks junior <eriksjunior13@gmail.com>
2022-08-03 22:21:59 -0300

    refactor

--- src/utils/DataToConfirmRegistration.ts
+++ src/utils/DataToConfirmRegistration.ts
@@ -1,14 +1,14 @@
 class DataToConfirmRegistration {
   
   getInformationFromEmail(receiver: string) {
-    const dataEMail = {
+    const dataEmail = {
       from: process.env.APP_EMAIL || "",
       to: receiver,
       subject: "Confirme seu email!",
       text: "olá, somos do promotionAPP por favor confirme seu email para utilizar nossa plataforma"
     }
 
-    return dataEMail
+    return dataEmail
   }
 
 }
--- src/utils/RegistrationConfirmation.ts
+++ src/utils/RegistrationConfirmation.ts
@@ -1,5 +1,4 @@
 import nodemailer from 'nodemailer'
-
 class RegistrationConfirmation {
   async Confirm(receiver: string) {
     const transporter = nodemailer.createTransport({
@@ -13,13 +12,12 @@ class RegistrationConfirmation {
 
     const result = await transporter.sendMail({
       from: process.env.APP_EMAIL,
-      to: receiver, //email do destinatario
+      to: receiver,
       subject: "Confirme seu email!",
       text: "olá, somos do promotionAPP por favor confirme seu email para utilizar nossa plataforma"
     })
 
-    // "responseCode": 250 = Essa resposta do servidor SMTP significa simplesmente que tudo correu bem e sua mensagem foi entregue ao servidor destinatário,
-    // "responseCode": 535 = deu merda,
+    
     return result
   }
 }
