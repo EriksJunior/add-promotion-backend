8a119d904521099958857008d20c6ff7719f5b4d
eriks junior <eriksjunior13@gmail.com>
2022-08-23 20:43:43 -0300

    feat: delete company after 7 days

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import cors from 'cors'
 import 'dotenv/config'
 import express from 'express'
 import { Router, Request, Response } from 'express';
+import knex from './config/database'
+
 
 import index from './routes/index'
 
@@ -10,6 +12,18 @@ const route = Router();
 
 app.use(cors())
 app.use(express.json())
+
+// setInterval(async () => {
+//   try {
+//     const teste =  knex.raw(`delete from company WHERE createdAt < date_sub( NOW(), INTERVAL 7 DAY) and id = 'be22169c-b4d7-44fe-8dd4-e852a5ddf450' and confirmed = 0`)
+//     console.log(teste.toQuery())
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }, 518400000)
+
+// 604800000, 7 days in ms
+
 app.use(route)
 app.use(index)
 
