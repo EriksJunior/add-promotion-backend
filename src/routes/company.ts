b49674c10b1ac255ebb2caf284cd10001df82b9f
eriks junior <eriksjunior13@gmail.com>
2022-08-30 21:15:19 -0300

    refactor

--- src/routes/company.ts
+++ src/routes/company.ts
@@ -1,29 +1,27 @@
 import { Request, Response, Router, NextFunction } from "express";
 
 import { companyController } from "../controllers";
-import HandleAuthorization from '../auth/HandleAuthorization'
 
 const router = Router();
 
-router.post('/createcompany', (request: Request, response: Response, next: NextFunction) => {
-  HandleAuthorization.auth();
-  // return companyController.create(request, response)
+router.post('/createcompany', (req: Request, res: Response, next: NextFunction) => {
+  return companyController.create(req, res)
 })
 
-router.get('/searchcompany/:id', (request, response) => {
-  return companyController.search(request, response)
+router.get('/searchcompany/:id', (req, res) => {
+  return companyController.search(req, res)
 })
 
-router.put('/updatecompany/:id', (request, response) => {
-  return companyController.update(request, response)
+router.put('/updatecompany/:id', (req, res) => {
+  return companyController.update(req, res)
 })
 
-router.get('/confirmed/email', (request, response) => {
-  return companyController.confirmUser(request, response)
+router.get('/confirmed/email', (req, res) => {
+  return companyController.confirmUser(req, res)
 })
 
-router.get('/resend/confirmation/email', (request, response) => {
-  return companyController.resendConfirmationEmail(request, response)
+router.get('/resend/confirmation/email', (req, res) => {
+  return companyController.resendConfirmationEmail(req, res)
 })
 
 router.delete('/deletecompany/:id', (request, response) => {
