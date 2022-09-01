b987be9a7d8a4757f279920654378816657fb5cc
eriks junior <eriksjunior13@gmail.com>
2022-08-31 22:14:16 -0300

    refactor

--- migrations/20220901011338_categories.ts
+++ migrations/20220901011338_categories.ts
@@ -1,13 +1,13 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('category', (table) =>{
+  return knex.schema.createTable('categories', (table) =>{
     table.increments('id').unsigned().primary()
     table.string('description')
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return await knex.schema.dropTable('category')
+  return await knex.schema.dropTable('categories')
 }
 
